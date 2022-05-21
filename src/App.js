d77009c25e70b904be1047d8551cbe27960932c6
NilangaVirajith <nilangavirajith5@gmail.com>
2022-05-22 03:51:45 +0530

    Order page creation started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
+import OrderTable from "./components/orders/OrderTable";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/orders" element={<OrderTable />} />
       </Routes>
     </Router>
   );
